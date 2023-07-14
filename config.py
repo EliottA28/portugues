56f687735d7d2080f58f05da663dfeb3c32a9125
Eliott Aubree <aubree.eliott@gmail.com>
2023-07-14 18:10:23 +0200

    update

---
+++ config.py
@@ -0,0 +1,15 @@
+KNOWN_LANGUAGED = "Français"
+LEARNED_LANGUAGED = "Portugais"
+
+
+NOTES = "Notes"
+TYPE = "Type"
+
+VERB = "verbe"
+ALL = "tous"
+NOUN = "nom"
+
+QUIZ = "Quiz"
+
+RANDOM = "aléatoire"
+WEIGHTED = ""
